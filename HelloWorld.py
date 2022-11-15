393d9565b3822532c8c7b9ce6bace16b7a2f5c8a
Preetiraj3697 <satendra11kum@gmail.com>
2022-11-15 22:28:38 +0530

    add

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,29 @@
 print("Hello world")
+#please dont remove this line
+# """
+# multi
+# linecache
+# ignore
+# now
+# """
+# """This is comment"""
+print("Hello world", "I am full stack web developer")
+print('C:\\Harry')
+print("Preeti is \n good boy \t1")
+var1 = "80"
+var4 = "58"
+var2 = 4
+var3 = 36.7
+print(var2+var3)
+print(int(var1)+int(var4))
+# str() int() float()
+print(10*"Hello world\n")
+print("Enter your number")
+# inpnum = input()
+# print("you entered",int(inpnum)*10);
+"""First calculator"""
+print("Enter first number")
+n1=input()
+print("Enter second number")
+n2=input()
+print("sum of these number is", int(n1) + int(n2))
